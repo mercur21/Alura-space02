2148322e2b17731016237ea6e66c53557260d650
mercur21 <vitor.eduardo2135@gmail.com>
2024-09-11 14:50:46 -0300

    Finalzinho de um dos cursos

--- galeria/models.py
+++ galeria/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+from datetime import datetime
+
+from django.contrib.auth.models import User
+
 class Fotografia(models.Model):
     
     OPCOES_CATEGORIA = [
@@ -13,8 +17,16 @@ class Fotografia(models.Model):
     legenda = models.CharField(max_length=150, null=False, blank=False)
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
     publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
+    usuario = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name="user",
+    )
     
     def __str__(self):
-        return f"Fotografia [nome={self.nome}]"
+        return self.nome
